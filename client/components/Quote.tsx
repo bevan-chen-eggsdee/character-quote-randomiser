2ddb6c2221a27554113c5091aabdc8d672463ef4
regie-malonzo <162513847+regie-malonzo@users.noreply.github.com>
2024-05-24 10:34:39 +1200

    randomise quotes

--- client/components/Quote.tsx
+++ client/components/Quote.tsx
@@ -1,6 +1,7 @@
 import request from 'superagent'
 import { useQuery } from '@tanstack/react-query'
 import { QuoteData } from '../../models/quotedata'
+import { useEffect, useState } from 'react'
 
 function useQuote() {
   return useQuery({
@@ -14,6 +15,27 @@ function useQuote() {
 
 export default function Quote() {
   const { data, isPending, isError, error } = useQuote()
+  const [randomQuote, setRandomQuote] = useState<{
+    dialog: string
+    character: string
+  } | null>(null)
+
+  useEffect(() => {
+    if (data?.docs) {
+      getRandomQuote()
+    }
+  }, [data]) //find more information about error!
+
+  const getRandomQuote = () => {
+    if (data?.docs.length) {
+      const randomIndex = Math.floor(Math.random() * data.docs.length)
+      const selectedDoc = data.docs[randomIndex]
+      setRandomQuote({
+        dialog: selectedDoc.dialog,
+        character: selectedDoc.character,
+      })
+    }
+  }
 
   if (isPending) {
     return <p>Loading...</p>
@@ -23,13 +45,16 @@ export default function Quote() {
   }
 
   return (
-    <>
-      <h1>QUOTE</h1>
-      <div>
-        {data.docs.map((quote) => (
-          <div key={quote.id}>{quote.dialog}</div>
-        ))}
-      </div>
-    </>
+    <div>
+      {randomQuote && (
+        <div>
+          <p>{randomQuote.dialog}</p>
+          <p>
+            <strong>{randomQuote.character}</strong>
+          </p>
+        </div>
+      )}
+      <button onClick={getRandomQuote}>Get Random Quote</button>
+    </div>
   )
 }
