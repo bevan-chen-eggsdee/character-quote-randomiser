402f1f5cda971b96b4ec211207680e31ea12f086
brad-craig <134657831+bradacraig@users.noreply.github.com>
2024-05-24 14:22:46 +1200

    created json data quotes characters

--- client/components/Character.tsx
+++ client/components/Character.tsx
@@ -1,17 +1,5 @@
-import request from 'superagent'
-import { useQuery } from '@tanstack/react-query'
-import { useState, useEffect } from 'react'
 import { CharacterInfo } from '../../models/characterData'
 
-// function useCharacter() {
-//   return useQuery({
-//     queryKey: ['character'],
-//     queryFn: async () => {
-//       const res = await request.get(`api/v1/character`)
-//       return res.body as CharacterDataList
-//     },
-//   })
-// }
 interface Props {
   randomCharacter: CharacterInfo
   getRandomCharacter: () => Promise<void>
